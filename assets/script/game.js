fb898c8597ca0cf94ea5d069e251db2d7d6441fa
danci <yangdanci@bettagames.com>
2023-04-07 14:24:33 +0800

    0.0.2

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,41 +1,21 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
-    },
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
-    start () {
 
     },
 
-    // update (dt) {},
+    onLoad(){
+        cc.log('onLoad')
+    },
+
+    clickButton:function(sender,str){//sender相当于调用函数的组件自身，str是customeventdata的传值
+        if(str=='pintu'){
+            cc.log('点击了拼图按钮')
+        }
+    },
+
+    update(dt){
+        cc.log('update')
+    },
 });
